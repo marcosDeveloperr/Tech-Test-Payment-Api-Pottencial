5e71dd188f456fa9de79c2094f554a1c2c7c32be
marcosDeveloperr <marcos.ti.profissional@gmail.com>
2023-01-25 10:22:22 -0300

    Iniciando o teste de tecnologia

---
+++ WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace Tech_Test_Payment_Api_Pottencial;
+
+public class WeatherForecast
+{
+    public DateTime Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
