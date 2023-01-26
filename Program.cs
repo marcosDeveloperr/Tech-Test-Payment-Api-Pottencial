6e8281883a5cfa9a832d116c1d96562977fd8299
marcosDeveloperr <marcos.ti.profissional@gmail.com>
2023-01-26 10:57:14 -0300

    add StringConexaoBanco

--- Program.cs
+++ Program.cs
@@ -1,6 +1,12 @@
+using Tech_Test_Payment_Api_Pottencial.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<VendaContext>(options => 
+options.UseSqlServer(builder.Configuration.GetConnectionString("StringConexaoBanco")));
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
